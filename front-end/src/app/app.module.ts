2c10a8503361234faa7010057b186f8118207f33
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2020-01-21 18:34:48 +0600

    model and sercvice successfully created

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
@@ -16,7 +17,8 @@ import { EmployeeListComponent } from './components/employee-list/employee-list.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
